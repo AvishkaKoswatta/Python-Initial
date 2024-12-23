cb51f9cf4f27d6ad20c773a890047b1f1b2bb76f
Avishka <avishkakkoswaththa@gmail.com>
2024-12-23 22:23:20 +0530

    twelth commit

--- Functions.py
+++ Functions.py
@@ -13,3 +13,11 @@ def shirts(size,text):
    print(f"This shirt size is {size} and {text} should be printed")
 
 shirts(size="xl",text="Hello, World")
+shirts("xxl","Hi")
+
+#dictionary
+def full_name(first,last):
+   dict={'first_name':first,'last_name':last}
+   return dict
+
+print('Anne', 'Marry')
