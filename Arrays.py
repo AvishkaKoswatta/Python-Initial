d98a37355fb5a1778530d83e794cc15103cd91ae
Avishka <avishkakkoswaththa@gmail.com>
2024-12-24 22:16:12 +0530

    seventhteen commit

---
+++ Arrays.py
@@ -0,0 +1,10 @@
+#Python does not have built-in support for Arrays
+#Python Lists
+#Python has a great built-in list type named "list".
+#List is a collection which is ordered and changeable. Allows duplicate members.
+#Lists are written with square brackets.
+#List items are ordered, changeable, and allow duplicate values.
+#List items are indexed, the first item has index [0], the second item has index [1] etc.
+#When we say that lists are ordered, it means that the items have a defined order, and that order will not change.
+#If you add new items to a list, the new items will be placed at the end of the list.
+
---
+++ Classes.py
@@ -0,0 +1,45 @@
+#Object-oriented programming in Python
+#Python is a multi-paradigm programming language. It supports different programming approaches.
+#One of the popular approaches to solve a programming problem is by creating objects. This is known as Object-Oriented Programming (OOP).
+#An object has two characteristics:
+#attributes
+#behavior
+#Let's take an example:
+#Parrot is an object,
+#name, age, color are attributes
+#singing, dancing are behavior
+#The concept of OOP in Python focuses on creating reusable code. This concept is also known as DRY (Don't Repeat Yourself).
+#In Python, the class is defined by using the class keyword.
+#A class is a blueprint for the object.
+#We can think of class as an sketch of a parrot with labels. 
+# It contains all the details about the name, colors, size etc. 
+# Based on these descriptions, we can study about the parrot. Here, a parrot is an object.
+#The example for class of parrot can be :
+#class Parrot:
+#    pass
+#Here, we use the pass keyword, which is generally used as a placeholder. It is a null statement in Python.
+#Classes in Python
+#Classes provide a means of bundling data and functionality together. 
+# Creating a new class creates a new type of object, allowing new instances of that type to be made. 
+# Each class instance can have attributes attached to it for maintaining its state. 
+# Class instances can also have methods (defined by its class) for modifying its state.
+#To create a class, use the keyword class:
+#Example
+#Create a class named MyClass, with a property named x:
+#class MyClass:
+#  x = 5
+#Create Object
+#Now we can use the class named MyClass to create objects:
+#Create an object named p1, and print the value of x:
+
+#p1 = MyClass()
+#print(p1.x)
+#The __init__() Function
+#The examples above are classes and objects in their simplest form, and are not really useful in real life applications.
+#To understand the meaning of classes we have to understand the built-in __init__() function.
+#All classes have a function called __init__(), which is always executed when the class is being initiated.
+
+class MyClass:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
