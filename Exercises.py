623e41a26e48380ec40e3a71164463437ac08ec6
Avishka <avishkakkoswaththa@gmail.com>
2024-12-26 14:22:15 +0530

    ninteenth commit

--- Exercises.py
+++ Exercises.py
@@ -24,3 +24,41 @@ for i in range(1,4):
     fact=fact*i
 
 print(fact)
+
+
+
+
+first=0
+second=1
+i=0
+while i<=20:
+    print(first)
+    next=first+second
+    first=second
+    second=next
+    i=i+1
+
+
+
+num=12345
+reversed_num=0
+while num>0:
+    digit=num%10
+    reversed_num=reversed_num*10+digit
+    num=num//10
+
+print("Reversed Number is: ",reversed_num)
+
+
+
+#Create a list
+numbers=input("Enter a number: ")
+numbers=int(numbers)
+num_list=[]
+i=0
+while i<numbers:
+    num=input("Enter numbers:")
+    num_list.append(num)
+    i+=1
+
+print(num_list)
--- Testing.py
+++
@@ -1,5 +0,0 @@
-fact=1
-for i in range(1,4):
-    fact=fact*i
-
-print(fact)
