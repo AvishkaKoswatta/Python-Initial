2b4717284b4ede8ac9d149c1d698281fc1e3f8d5
Avishka <avishkakkoswaththa@gmail.com>
2025-01-19 20:40:45 +0530

    selection sort

--- Algorithms.py
+++ Algorithms.py
@@ -1,14 +1,29 @@
 #Sorting Algorithms
+
 # Buble sort
 # compares adjacent elements and swaps them
-def bubble_sort(arr):
+# def bubble_sort(arr):
+#     n=len(arr)
+#     for i in range (n):
+#         for j in range (n-i-1):
+#             if arr[j]>arr[j+1]:
+#                 arr[j],arr[j+1]=arr[j+1], arr[j]
+    
+#     return arr
+
+# print(bubble_sort([1,8,4,3,9,0,12,7]))
+
+
+
+# Selection sort
+def selection_sort(arr):
     n=len(arr)
     for i in range (n):
-        for j in range (n-i-1):
-            if arr[j]>arr[j+1]:
-                arr[j],arr[j+1]=arr[j+1], arr[j]
-    
+        min_idx = i
+        for j in range (i+1,n):
+            if arr[min_idx] > arr[j]:
+                min_idx=j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
     return arr
 
-print(bubble_sort([1,8,4,3,9,0,12,7]))
-
+print(selection_sort([1,4,2,-9,34,9,12]))
