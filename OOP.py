8fb543fc3af01a19d766af4f995dacc1cf467d85
Avishka <avishkakkoswaththa@gmail.com>
2025-01-21 22:39:13 +0530

    encapsulation

---
+++ OOP.py
@@ -0,0 +1,37 @@
+class BankAccount:
+    def __init__(self, accountType,amount, withdraw, deposit):
+        self.accountType=accountType
+        self.__amount=float(amount)
+        self.withdraw=float(withdraw)
+        self.deposit=float(deposit)
+            
+    def description(self):
+        print("This is a "+ self.accountType)
+    
+    def getBalance(self):
+        return self.__amount
+
+    def setBalance(self, amount):
+        self.__amount=amount
+
+    def withdrawMoney(self):
+        if self.__amount>self.withdraw:
+            self.__amount=self.__amount-self.withdraw
+            print(f"After withdraw total amount is { self.__amount} ")
+        else:
+            print("Insufficient balance.")
+    
+    def depositMoney(self):
+        self.__amount=self.__amount+self.deposit
+        print(f"After deposit total amount is {self.__amount}")
+
+account1=BankAccount("savings", 100,10,20)
+print(f"Initial balance: {account1.getBalance()}")
+account1.withdrawMoney()
+account1.depositMoney()
+
+
+# Encapsulation
+# name - public
+# _name - protected
+# __name - private
