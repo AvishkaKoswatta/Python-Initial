d916f555f8f79a369070fce4dc6b4792c5187b0e
Avishka <avishkakkoswaththa@gmail.com>
2025-01-29 22:38:45 +0530

    exercise-leap year

--- Testing.py
+++ Testing.py
@@ -1,5 +1,7 @@
-x="helloworld"
-#print(x[1:5])  
-print(x[:-1])
-# print(x[3:])
-# print(x[-6:-2]) 
+# x="helloworld"
+# #print(x[1:5])  
+# print(x[:-1])
+# # print(x[3:])
+# # print(x[-6:-2]) 
+
+
