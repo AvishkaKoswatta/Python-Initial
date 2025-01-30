c07106ae0a3fdeb23636fe198ff80ca0af8f1ef3
Avishka <avishkakkoswaththa@gmail.com>
2025-01-30 22:36:24 +0530

    nochanges

--- Testing.py
+++ Testing.py
@@ -5,3 +5,22 @@
 # # print(x[-6:-2]) 
 
 
+# def maximum_value():
+#     # Read K and M
+#     K, M = map(int, input().split())  # Convert input to integers
+#     y = []  # List to store squared max values
+    
+#     # Read K lists
+#     for _ in range(K):
+#         lst = list(map(int, input().split()[1:]))  # Read list and ignore the first element
+#         y.append(max(lst) ** 2)  # Square the max value and add to y
+    
+#     # Compute and return the result
+#     return sum(y) % M
+
+# # Call function and print result
+# print(maximum_value())
+
+
+
+
