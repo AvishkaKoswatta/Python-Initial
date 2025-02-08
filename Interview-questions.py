ddeecca6ecd0203c6135d239c86faa5b4218d583
Avishka <avishkakkoswaththa@gmail.com>
2025-02-08 21:00:41 +0530

    interview-questions

---
+++ Interview-questions.py
@@ -0,0 +1,7 @@
+# Python interview questions
+
+#What is the difference between a Mutable datatype and an Immutable data type?
+#   Mutable data types can be changed after they are created
+#   Immutable data types can not be changed after they are created.
+
+#
